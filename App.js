477ab83f3c31a7cb2c6f48d943231e02e6d59fd1
eduar766 <eduar766@gmail.com>
2025-01-14 17:37:11 -0300

    Initialize project structure with configuration files, assets, and basic styling

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, View } from "react-native";
 import { MainApp } from "./components/MainApp";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import "./global.css";
+
 export default function App() {
   return (
     <SafeAreaProvider>
